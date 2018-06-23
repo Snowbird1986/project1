50bb7634e1588edc02ed121e49f27f809cf11bd0
Snowbird <jakounter@yahoo.com>
2018-06-23 11:28:15 -0500

    firebase

---
+++ assets/javascript/project1.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDsZcPe2YbNAVIrKlgji6lu4t7ftUU18D0",
+    authDomain: "project1-1529767008392.firebaseapp.com",
+    databaseURL: "https://project1-1529767008392.firebaseio.com",
+    projectId: "project1-1529767008392",
+    storageBucket: "project1-1529767008392.appspot.com",
+    messagingSenderId: "534435451804"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+});
